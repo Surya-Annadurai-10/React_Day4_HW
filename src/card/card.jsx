338d3ebe552c3867e1f8099d67245d66e2045447
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-12-20 00:50:46 +0530

    task Completed

---
+++ src/card/card.jsx
@@ -0,0 +1,33 @@
+import styles from './card.module.css'
+
+
+const Card = (props) =>{
+    console.log(props);
+    const onCopy = (ele) =>{
+     console.log(ele.id);
+     const textToCopy = `#${ele.colorCode}`
+     navigator.clipboard.writeText(textToCopy)
+     .then(() =>{
+        
+     alert("Copied");
+        
+     }).catch(() =>{
+        alert("Failed to Copy");
+     })
+        
+    }
+  return <>
+   <div className={styles.container}>
+     <div style={{backgroundColor: `#${props.colorCode}`}} className={styles.color_box}></div>
+     <div className={styles.down_box}>
+        <h2>#{props.colorCode}</h2>
+        <h3 style={{color: `#${props.colorCode}`}}>{props.colorName}</h3><span>  <button onClick={() =>{onCopy(props)}} className={styles.btn}>Copy</button></span>
+    </div>
+  
+
+</div>
+  </>
+  
+}
+
+export default Card;
